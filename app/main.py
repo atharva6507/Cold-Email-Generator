4e07659b2bbaa8a49a1778ac964a809bb240cf6b
Atharva Raut <75558547+atharva6507@users.noreply.github.com>
2024-12-15 17:54:32 +0530

    changes

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from portfolio import Portfolio
 from utils import clean_text
 
 
-def create_streamlit_app(llm, portfolio, clean_text):
+def create_streamlit_app(chain_instance, portfolio_instance, text_cleaner):
     st.title("📧 Cold Mail Generator")
     url_input = st.text_input("Enter a URL:")
     submit_button = st.button("Submit")
@@ -14,13 +14,13 @@ def create_streamlit_app(llm, portfolio, clean_text):
     if submit_button:
         try:
             loader = WebBaseLoader([url_input])
-            data = clean_text(loader.load().pop().page_content)
-            portfolio.load_portfolio()
-            jobs = llm.extract_jobs(data)
+            data = text_cleaner(loader.load().pop().page_content)
+            portfolio_instance.load_portfolio()
+            jobs = chain_instance.extract_jobs(data)
             for job in jobs:
                 skills = job.get('skills', [])
-                links = portfolio.query_links(skills)
-                email = llm.write_mail(job, links)
+                links = portfolio_instance.query_links(skills)
+                email = chain_instance.write_mail(job, links)
                 st.code(email, language='markdown')
         except Exception as e:
             st.error(f"An Error Occurred: {e}")
